593d98ab955cb9ab2aca895dd0e4dca5b0e01cb3
lightTAKE <tadas.kekys@gmail.com>
2023-01-29 14:51:01 +0200

    added language detector

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,6 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import en_translation from "./en/translations.json";
 import lt_translation from "./lt/translations.json";
 
@@ -12,12 +13,35 @@ export const resources = {
   },
 };
 
-i18next.use(initReactI18next).init({
-  lng: "lt",
-  debug: true,
-  resources,
-  react: {
-    transSupportBasicHtmlNodes: true,
-    transKeepBasicHtmlNodesFor: ["br", "strong", "i"],
+const languageDetector = new LanguageDetector();
+languageDetector.addDetector({
+  name: "languageFromPath",
+  lookup(): string | string[] | undefined {
+    const queryParams = new URLSearchParams(document.location.search);
+    const language = queryParams.get("language");
+    switch (language) {
+      case "en":
+        return "en";
+      case "lt":
+        return "lt";
+      default:
+        return undefined;
+    }
   },
 });
+
+i18next
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init({
+    detection: {
+      order: ["languageFromPath"],
+    },
+    fallbackLng: "en",
+    debug: true,
+    resources,
+    react: {
+      transSupportBasicHtmlNodes: true,
+      transKeepBasicHtmlNodesFor: ["br", "strong", "i"],
+    },
+  });
